c76588a93c5549e0d79afd619e4468384aaedb77
EQKim <kareem10kareem@gmail.com>
2025-11-10 13:19:19 +0100

    Clean up
     * Old deprecated code - AI Player logic
     * Added gitigonore file as well

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Lobby from "./ui/Lobby.vue";
-import OnlineBoard from "./services/OnlineBoard.vue"; // ensure this path matches your file
+import OnlineBoard from "./services/OnlineBoard.vue"; 
 
 const roomId = ref<string | null>(null);
 const isHost = ref(false);
--- src/game/Player.ts
+++
@@ -1,9 +0,0 @@
-import type { Card } from "../cards/Card"
-
-export type PlayerId = string
-export type Player = {
-  id: PlayerId
-  name: string
-  hand: Card[]
-  isAI?: boolean
-}
